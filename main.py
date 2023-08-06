5f079061bd4fce2b074bf975bb6dde33a0e066df
PurnaMC <139571395+PurnaMC@users.noreply.github.com>
2023-08-07 01:12:49 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,40 @@
+import streamlit as st
+import pandas
+
+st.set_page_config(layout="wide")
+
+col1, col2 = st.columns(2)
+
+with col1:
+    st.image("images/photo.png",)
+
+with col2:
+    st.title("Purna Chandra M")
+    content = """
+    hi, I am Purna! I am a Python programmer, teacher, and founder of PythonHow. I graduated in 2022 from D.N.R College 
+    of Engineering & Technology studied Electronis and enginnering technology .i learned full stack dev with css,html,
+    javascript ,python etc. and i made some applications with python 
+    """
+    st.info(content)
+
+content2 = """
+Below you can find some of the apps I have build in Python. Feel free to contact me!"""
+
+st.write(content2)
+
+col3, empty_col, col4 = st.columns([1.5, 0.5, 1.5])
+
+df = pandas.read_csv("data.csv", sep=";")
+with col3:
+    for index, row in df[:10].iterrows():
+        st.header(row["title"])
+        st.write(row["description"])
+        st.image("images/" + row["image"])
+        st.write(f"[Source Code]({row['url']})")
+
+with col4:
+    for index, row in df[10:].iterrows():
+        st.header(row["title"])
+        st.write(row["description"])
+        st.image("images/" + row["image"])
+        st.write(f"[Source Code]({row['url']})")
